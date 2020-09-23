d676207ce0b412fb75d2f469c7f9536d3786a587
Anne Nardolilli <anardolilli@gmail.com>
2020-09-23 21:40:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,6 +4,8 @@ def game_hash
      :team_name => "Brooklyn Nets",
      :colors => ["Black", "White"],
      :players => [
+       {:player_name => "",
+       :number =>
        
        
        ]
